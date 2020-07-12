60db4af1e68d7185431d7375333144452d232538
Matheu Soto <matheu.soto.dev@gmail.com>
2020-07-12 03:31:36 -0500

    -

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ const path = require('path')
 const productionHost = 'https://zero-react.vercel.app/'
 const nameApp = 'Zero Start'
 
+const nodeEnv = process.env.NODE_ENV.trim()
+console.log(nodeEnv)
+
 const browserConfig = {
   entry: './src/index.js',
   output: {
@@ -65,7 +68,48 @@ const browserConfig = {
       filename: 'build/css/main.css'
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, '/src/assets/static/index.html')
+      template: path.join(__dirname, nodeEnv === 'production' ? '/src/assets/static/index.production.html' : '/src/assets/static/index.development.html')
+    }),
+    nodeEnv === 'production' ? new WebpackPwaManifestPlugin({
+      name: nameApp,
+      ios: true,
+      inject: true,
+      orientation: 'portrait',
+      display: 'standalone',
+      start_url: productionHost + 'index.html',
+      shortname: nameApp,
+      description: nameApp,
+      background_color: '#fff',
+      theme_color: '#fff',
+      icons: [
+        {
+          src: path.resolve('src/assets/img/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('icons', 'ios'),
+          ios: true
+        }
+      ]
+    }) : false,
+    nodeEnv === 'production' ? new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(productionHost),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp(productionHost),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
+    }) : false,
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer'
     })
   ].filter(Boolean)
 }
